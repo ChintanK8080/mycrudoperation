ff8ff4f9d1473180c7c76e43ad7b7fd7355acb98
ChintanK <chintank8080@gmail.com>
2022-11-20 02:09:57 -0500

    i don't  what i did

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render,redirect
 from home.models import Login
 from django.contrib import messages
 
@@ -9,20 +9,21 @@ def index(request):
 
 
 def login(request):
-    if(request.method == 'POST'):
-        name = request.POST.get('name')
-        address = request.POST.get('address')
-        phone = request.POST.get('phone')
-        age = request.POST.get('age')
-        login = Login(name=name,address=address,age=age,phone=phone)
-        login.save()
-        messages.success(request,"profile updated successfully")
     return render(request, 'login_page.html')
 
 
 
 
 def signup(request):
+    if(request.method == 'POST'):
+       name = request.POST.get('name')
+       address = request.POST.get('address')
+       phone = request.POST.get('phone')
+       age = request.POST.get('age')
+       login = Login(name=name,address=address,age=age,phone=phone)
+       login.save()
+       messages.success(request,"profile updated successfully")
+       return redirect('/login') 
     return render(request, 'signup_page.html')
 
 
